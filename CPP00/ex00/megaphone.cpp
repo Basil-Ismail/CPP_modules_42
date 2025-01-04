41963b4edd03b8efb8e072566b0d93cbfd7c7d7a
Basil Ismail <bismail@c2r3s1.42amman.com>
2025-01-04 17:11:07 +0300

    Solved Ex00 in cpp, by a noob, YAY

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int	main(int ac, char **av)
+{
+	std::string str;
+        if(ac != 1){
+	for (int i = 1; i < ac; i++)
+	{
+		str = av[i];
+		for (int j = 0; av[i][j]; j++)
+			str[j] = toupper(str[j]);
+		std::cout << str;
+	}
+        }else
+                std::cout << "* LOUD AND UBEARBLE FEEDBACK NOISE * ";
+        std::cout<<"\n";
+}
