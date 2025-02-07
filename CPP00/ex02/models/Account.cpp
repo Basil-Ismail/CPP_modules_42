60b6d50191b55a49f356b3eb62a01c4691a7f21d
Abueskander <basselawni1@gmail.com>
2025-02-07 17:06:38 +0300

    Finishde a couple of functions and fixed some weirds shit errors :D

--- CPP00/ex02/models/Account.cpp
+++ CPP00/ex02/models/Account.cpp
@@ -1,12 +1,19 @@
 #include "Account.hpp"
+#include <ctime>
+#include <iostream>
+#define DAY_BUFFER 18
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 Account::Account(int initial_deposit)
 {
-        this->_accountIndex = 0;
-        this->_amount = 0;
-        this->_nbDeposits = 0;
-        this->_nbWithdrawals = 0;
-        (void)initial_deposit;
+        _displayTimestamp();
+        _accountIndex = _nbAccounts++;
+        std::cout << "index:" << _accountIndex << ";amount:" << initial_deposit << ";created\n";
+        _totalAmount += initial_deposit;
+        _amount = initial_deposit;
 }
 Account::~Account()
 {
@@ -24,8 +31,14 @@ int Account::getNbWithdrawals()
 {
         return (_totalNbWithdrawals);
 }
+int Account::getNbDeposits()
+{
+        return (_totalNbDeposits);
+}
 void Account::displayAccountsInfos()
 {
+        _displayTimestamp();
+        std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawls" << _totalNbWithdrawals << std::endl;
 }
 
 void Account::makeDeposit(int deposit)
@@ -45,5 +58,24 @@ void Account::displayStatus() const
 {
 }
 void Account::_displayTimestamp()
+// This function is a function that prints out the timestamp of the day,
+//  if there is no system time, it will dispaly [N/A]
 {
+        std::time_t timeObj = time(NULL);
+        char timelog[DAY_BUFFER];
+        struct std::tm *lt;
+
+        if (timeObj == -1)
+        {
+                std::cout << "[N/A]" << std::endl;
+                return;
+        }
+        lt = std::localtime(&timeObj);
+        if (!lt)
+        {
+                std::cout << "[N/A]" << std::endl;
+                return;
+        }
+        std::strftime(timelog, DAY_BUFFER, "[%Y%m%d_%H%M%S]", lt);
+        std::cout << timelog;
 }
