f74d6080cfa063102de1afd22049ac4f93846be0
Abueskander <basselawni1@gmail.com>
2025-02-08 20:14:11 +0300

    CPP01-Ex02 is done

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(){
+  std::string first("HI THIS IS BRAIN");
+  std::string *stringPTR = &first;
+  std::string &stringREF = first;
+  
+  std::cout << &first <<std::endl;
+  std::cout << stringPTR<< std::endl;
+  std::cout << &stringREF <<std::endl;
+  
+  std::cout << first <<std::endl;
+  std::cout << *stringPTR<<std::endl;
+  std::cout <<stringREF <<std::endl;
+}
