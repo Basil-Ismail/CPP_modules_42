4d1355f983430861e2cbc628c1f030df9f25450e
Abueskander <basselawni1@gmail.com>
2025-02-14 18:54:04 +0300

    Continuing on Ex04, Boring as hell

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
+bool argcsErrors(int argc, char **argv)
+{
+	(void)argv;
+	if (argc != 4)
+	{
+		std::cerr << "Not enough args" << std::endl;
+		return true;
+	}
+	return false;
+}
+
+std::string replace(std::string &fileContent, std::string old, std::string newer)
+{
+	std::string newString = fileContent;
+	while (newString.find(fir))
+}
 int main(int argc, char **argv)
 {
-  (void)argc;
-  (void)argv;
-  return 0;
+
+	if (argcsErrors(argc, argv))
+		return 1;
+	std::ifstream Myfile(argv[1]);
+	if (!Myfile.is_open())
+	{
+		std::cerr << "File is not valid or can't be opened " << std::endl;
+		return 2;
+	}
+	std::stringstream buffer;
+	buffer << Myfile.rdbuf();
+	std::string fileContent = buffer.str();
+	fileContent = replace(fileContent, argv[2], argv[3]);
+	std::cout << fileContent;
 }
