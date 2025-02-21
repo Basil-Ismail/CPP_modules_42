745f38da7a1e2ebd3f927e2a921879d1c87bf7a4
Abueskander <basselawni1@gmail.com>
2025-02-21 23:25:28 +0300

    Double checked the print statments

--- CPP01/ex05/models/Harl.cpp
+++ CPP01/ex05/models/Harl.cpp
@@ -2,25 +2,29 @@
 
 void Harl::debug()
 {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "RTFM!!!" << std::endl;
 }
 void Harl::info()
 {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "This is so Informative <3" << std::endl;
 }
 void Harl::warning()
 {
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "Get the F out of here!!! " << std::endl;
 }
 void Harl::error()
 {
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now!!" << std::endl;
 }
 
 void Harl::complain(std::string mode)
 {
     void (Harl::* array[])() = { &Harl::error, &Harl::warning, &Harl::info, &Harl::debug };
-    std::string modes[4] = { "error", "warning", "info", "debug" };
+    std::string modes[4] = { "ERROR", "WARNING", "INFO", "DEBUG" };
     for (size_t i = 0; i < 4; i++) {
         if (mode == modes[i]) {
             (this->*array[i])();
