504a9195df62a4da87b46a600d85d6d854ffa9a1
Abueskander <basselawni1@gmail.com>
2025-02-23 00:20:55 +0300

    Fixes overall and fixed some bugs

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -1,51 +1,50 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
 bool argcsErrors(int argc)
 {
-	if (argc != 4)
-	{
-		std::cerr << "Not enough args" << std::endl;
-		return true;
-	}
-	return false;
+    if (argc != 4) {
+        std::cerr << "Not enough args" << std::endl;
+        return true;
+    }
+    return false;
 }
 
-std::string replace(std::string &fileContent, std::string old, std::string newer)
+std::string replace(std::string& fileContent, std::string old, std::string newer)
 {
-	std::string newString;
-	int i;
+    int i = 0;
 
-	while (old.length() && (i = fileContent.find(old)) != -1)
-	{
-		fileContent.erase(i, old.length());
-		fileContent.insert(i, newer);
-	}
-	return (fileContent);
+    while (old.length() && (i = fileContent.find(old, i)) != -1) {
+        fileContent.erase(i, old.length());
+        fileContent.insert(i, newer);
+        i += newer.length();
+    }
+    return (fileContent);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 
-	if (argcsErrors(argc))
-		return 1;
-	std::ifstream Myfile(argv[1]);
-	if (!Myfile.is_open())
-	{
-		std::cerr << "File is not valid or can't be opened " << std::endl;
-		return 2;
-	}
-	std::stringstream buffer;
-	buffer << Myfile.rdbuf();
-	std::string fileContent = buffer.str();
-	if (fileContent.empty())
-		std::cerr << "Empty File." << std::endl;
-	else
-	{
-		fileContent = replace(fileContent, argv[2], argv[3]);
-		std::cout << fileContent;
-	}
-	Myfile.close();
-	return (0);
+    if (argcsErrors(argc))
+        return 1;
+    std::ifstream Myfile(argv[1]);
+    if (!Myfile.is_open()) {
+        std::cerr << "File is not valid or can't be opened " << std::endl;
+        return 2;
+    }
+    std::string newName(argv[1]);
+    std::ofstream newFile(newName.append(".replace").c_str());
+    std::stringstream buffer;
+
+    buffer << Myfile.rdbuf();
+    std::string fileContent = buffer.str();
+    if (fileContent.empty())
+        std::cerr << "Empty File." << std::endl;
+    else {
+        fileContent = replace(fileContent, argv[2], argv[3]);
+        newFile << fileContent;
+    }
+    Myfile.close();
+    return (0);
 }
