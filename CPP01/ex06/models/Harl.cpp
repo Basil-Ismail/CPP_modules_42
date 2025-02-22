dfdb9259de7cde116b879d8f62a948ed1e12e98b
Abueskander <basselawni1@gmail.com>
2025-02-23 00:28:42 +0300

    Fixed overall things to make it more accurate

--- CPP01/ex06/models/Harl.cpp
+++ CPP01/ex06/models/Harl.cpp
@@ -3,22 +3,26 @@
 void Harl::debug()
 {
     std::cout << "[ DEBUG ]" << std::endl;
-    std::cout << "RTFM!!!" << std::endl;
+    std::cout << "RTFM!!!\n"
+              << std::endl;
 }
 void Harl::info()
 {
     std::cout << "[ INFO ]" << std::endl;
-    std::cout << "This is so Informative <3" << std::endl;
+    std::cout << "This is so Informative <3\n"
+              << std::endl;
 }
 void Harl::warning()
 {
     std::cout << "[ WARNING ]" << std::endl;
-    std::cout << "Get the F out of here!!! " << std::endl;
+    std::cout << "Get the F out of here!!! \n"
+              << std::endl;
 }
 void Harl::error()
 {
     std::cout << "[ ERROR ]" << std::endl;
-    std::cout << "This is unacceptable! I want to speak to the manager now!!" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now!!\n"
+              << std::endl;
 }
 
 void Harl::complain(std::string mode)
@@ -27,30 +31,25 @@ void Harl::complain(std::string mode)
     std::string modes[4] = { "ERROR", "WARNING", "INFO", "DEBUG" };
     size_t i = 0;
 
-    for (i = 0; i < 4; i++) {
-        if (mode == modes[i]) {
+    for (i = 0; i < 4; i++)
+        if (mode == modes[i])
             break;
-        }
-    }
+
     switch (i) {
     case 3:
         (this->*array[3])();
-        std::cout << std::endl;
         // fall through
     case 2:
         (this->*array[2])();
-        std::cout << std::endl;
         // fall through
     case 1:
         (this->*array[1])();
-        std::cout << std::endl;
         // fall through
     case 0:
         (this->*array[0])();
-        std::cout << std::endl;
         break;
     default:
-        std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
+        std::cerr << "No valid mode " << std::endl;
         break;
     }
 }
